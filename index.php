70d5fa7bedadf710ad3100e75e013d4e7a4940bb
IcedFREELANCER <freelancer@mail.ua>
2019-06-07 21:15:04 +0300

    третий комит 7.06

--- index.php
+++ index.php
@@ -15,27 +15,40 @@ echo $c--, "<br>";
 echo --$d, "<br>";
 
 
-
-
 $d = "Hello";
-$d = $d. "world";
+$d = $d . "world";
 echo $d;
 echo "<br>";
 $d .= "! ! !";
 echo $d;
 
 
-
 $value = 10;
 $value = $value + 5;
 $value += 5;
-echo $value , "<br>";
+echo $value, "<br>";
 $value -= 5;
-echo $value , "<br>";
+echo $value, "<br>";
 $value *= 5;
-echo $value , "<br>";
+echo $value, "<br>";
 $value /= 5;
-echo $value , "<br>";
+echo $value, "<br>";
+
+$a = 45;
+switch ($a) {
+    case $a <= 50:
+        echo "норма";
+        break;
+    case $a >= 50:
+        echo "много";
+        break;
+    case $a >= 90:
+        echo "очень много";
+        break;
+    default :
+        echo "не найдено";
+        break;
+}
 
 ?>
 

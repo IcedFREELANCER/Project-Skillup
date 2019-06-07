9b6af5d4a8970c0f9e9b00b387bf59677a62529a
IcedFREELANCER <freelancer@mail.ua>
2019-06-07 20:14:41 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello world</title>
+
+</head>
+<body>
+
+<?php
+
+$a = $b = $c = $d = 2;
+echo $a++, "<br>";
+echo ++$b, "<br>";
+echo $c--, "<br>";
+echo --$d, "<br>";
+
+
+
+
+$d = "Hello";
+$d = $d. "world";
+echo $d;
+echo "<br>";
+$d .= "! ! !";
+echo $d;
+
+
+
+$value = 10;
+$value = $value + 5;
+$value += 5;
+echo $value;
+$value -= 5;
+echo $value;
+$value *= 5;
+echo $value;
+$value /= 5;
+echo $value;
+
+?>
+
+</body>
+</html>
